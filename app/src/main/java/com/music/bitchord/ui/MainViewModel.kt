9e6c46ba39b8177e80001ca7ec4681b15181f040
chahaapk <chahaapk@users.noreply.github.com>
2026-09-13 22:31:49 +0530

    fix: add missing Log import in MainViewModel

--- app/src/main/java/com/music/bitchord/ui/MainViewModel.kt
+++ app/src/main/java/com/music/bitchord/ui/MainViewModel.kt
@@ -1,6 +1,7 @@
 package com.music.bitchord.ui
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import com.music.bitchord.R
